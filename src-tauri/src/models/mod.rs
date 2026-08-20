7ee42d65f52056eba079d4c8c43c1b72f6156759
hayal05 <hayelommengistu@gmail.com>
2026-08-20 10:18:51 +0300

    feat: register expense models

--- src-tauri/src/models/mod.rs
+++ src-tauri/src/models/mod.rs
@@ -1,9 +1,11 @@
 pub mod category;
+pub mod expense;
 pub mod item;
 pub mod raw_material;
 pub mod waiter;
 
 pub use category::Category;
+pub use expense::{Expense, NewExpense};
 pub use item::Item;
 pub use raw_material::{RawMaterial, RawMaterialPurchase};
 pub use waiter::Waiter;
